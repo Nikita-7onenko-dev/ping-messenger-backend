597a83ba22f380d099de9ee1e8eca344b97a1e96
Nikita Simonenko <nikitatrihomkin@gmail.com>
2026-08-20 18:37:13 +0300

    chore: configure database connection

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,32 @@
 import express from "express";
+import { pool } from "./database/database.config.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 
-app.get("/ping", (_, res) => 
-  res.json({ message: "pong" })
-);
-
+app.get("/ping", (_, res) => res.json({ message: "pong" }));
 
 async function startApp() {
   console.log("Starting server...");
-  console.log(`On PORT: ${PORT}`);
+  console.log(`PORT: ${PORT}`);
 
   try {
+    await pool.query("SELECT 1");
+    console.log("Database connected successfully");
 
     const server = app.listen(PORT, () => {
       console.log(`RUN SERVER ON PORT ${PORT}`);
       console.log("Come GET /some!");
-    })
+    });
 
     server.on("error", (err) => {
       console.log("Failed to start app:", err);
-    })
-
-  } catch(error) {
+    });
+  } catch (error) {
     console.log("Failed to start app:", error);
     process.exit(1);
   }
-};
+}
 
 startApp();
